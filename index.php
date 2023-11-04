9c51fbb8c317af579919355d1a6c02169b8497c2
Luca Garofalo <79308097+Lucksi@users.noreply.github.com>
2023-11-04 15:46:33 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -10,11 +10,19 @@ License: GNU General Public License v3.0-->
             function get_res(){
                 if (isset($_POST["WEB"])){
                     $parameter = $_POST["input"];
+                    if($parameter == ""){
+                        echo"<script>alert('Insert a parameter')</script>";
+                        echo "<span id = 'NotFound'>Insert a parameter</span>";
+                    }
                     $filename = "output/{$parameter}.txt";
                     $filename2 = "output/{$parameter}.Dk";
                 }
-                else{
+                else if(isset($_POST["IMAGE"])){
                     $parameter = $_POST["input"];
+                    if($parameter == ""){
+                        echo"<script>alert('Insert a parameter')</script>";
+                        echo "<span id = 'NotFound'>Insert a parameter</span>";
+                    }
                     $filename = "output/{$parameter}_image.txt";
                     $filename2 = "output/{$parameter}_image.Dk";
                 }
@@ -45,54 +53,58 @@ License: GNU General Public License v3.0-->
                         echo "<span id = 'NotFound'>{$parameter} Not Found</span>";
                     }
                 }
-                else{
-                    echo"<script>alert('Insert a parameter')</script>";
-                    echo "<span id = 'NotFound'>Insert a parameter</span>";
-                }
             }
         ?>
         <script>
             function Image_Search(){
                 document.getElementById("image").style.display="block";
                 document.getElementById("Web").style.display="none";
+                document.getElementById("Img").style.backgroundColor="Green";
+                document.getElementById("Img").style.borderRadius="20px";
+                document.getElementById("Img").style.width="60px";
+                document.getElementById("web").style.width="none";
+                document.getElementById("web").style.backgroundColor="transparent";
+                //document.getElementsByClassName("choice_bar")[0].style.marginLeft="-25px";
             }
             function Web_Search(){
-                document.getElementById("image").style.display="none";
                 document.getElementById("Web").style.display="block";
-            }   
+                document.getElementById("image").style.display="none";
+                document.getElementById("web").style.backgroundColor="Green";
+                document.getElementById("web").style.borderRadius="20px";
+                document.getElementById("web").style.width="50px";
+                document.getElementById("Img").style.width="none";
+                document.getElementById("Img").style.backgroundColor="transparent";
+            }
         </script>
         <title>Darkus</title>
         <link rel = "stylesheet" href ="Css/Style.css">
     </head>
-    <body>
+    <body onload = "Web_Search()">
         <center>
             <h1>DARKUS</h1>
             <p>A Onion Link Searcher</p>
             <div class = "choice_bar">
-                <h5 onclick="Image_Search()">Images</h5>
-                <h5 onclick="Web_Search()">Web</h5>   
-            </div>         
+                <p onclick="Image_Search()" id = "Img">Images</p>
+                <p onclick="Web_Search()" id = "web">Web</p>
+            </div>
             <div class = "searchbar">
                 <div id ="Web">
-                    <form action = "" method = "Post">
-                        <input type = "text" placeholder = "Insert the term to search in the DB" name = "input" id = "search">
+                    <form action = "" method = "POST">
+                        <input type = "text" placeholder = "Insert the parameter to search in the DB" name = "input" id = "search">
                         <button type = "submit" name = "WEB">Search</button>
                     </form>
                 </div>
                 <div id ="image">
-                    <form action = "" method = "Post">
-                        <input type = "text" placeholder = "Insert the image to search in the DB" name = "input" id = "search">
+                    <form action = "" method = "POST">
+                        <input type = "text" placeholder = "Insert the image parameter to search in the DB" name = "input" id = "search">
                         <button type = "submit" name = "IMAGE">Search</button>
-                </div>     
-                </form>
+                    </form>
+                </div>
                 <?php
                     get_res();
-                ?>   
+                ?>
             </div>
-        <!--</center>-->
         </center>
         </div>
     </body>
 </html>
-
-<?php
